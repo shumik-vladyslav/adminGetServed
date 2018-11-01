ee7db3de24246ed8b609e421e9f6a323b54d3c3c
Vladyslav <shumik.vladyslav@gmail.com>
2018-11-01 16:07:10 +0200

    first

---
+++ src/app/main/pages/errors/500/error-500.component.ts
@@ -0,0 +1,40 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+import { FuseConfigService } from '@fuse/services/config.service';
+
+@Component({
+    selector     : 'error-500',
+    templateUrl  : './error-500.component.html',
+    styleUrls    : ['./error-500.component.scss'],
+    encapsulation: ViewEncapsulation.None
+})
+export class Error500Component
+{
+    /**
+     * Constructor
+     *
+     * @param {FuseConfigService} _fuseConfigService
+     */
+    constructor(
+        private _fuseConfigService: FuseConfigService
+    )
+    {
+        // Configure the layout
+        this._fuseConfigService.config = {
+            layout: {
+                navbar   : {
+                    hidden: true
+                },
+                toolbar  : {
+                    hidden: true
+                },
+                footer   : {
+                    hidden: true
+                },
+                sidepanel: {
+                    hidden: true
+                }
+            }
+        };
+    }
+}
